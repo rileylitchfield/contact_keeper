a28280044c80b029b5516e9bee0e417174dcc4a3
Riley <rileylitchfield95@gmail.com>
2020-10-13 20:57:37 -0500

    Added JSON web token and authentication route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
-const User = require('../models/User')
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
@@ -36,7 +38,18 @@ router.post('/', [
     user.password = await bcrypt.hash(password, salt);
     await user.save();
 
-    res.send("User saved");
+    const payload = {
+      user: {
+        id: user.id 
+      }
+    }
+
+    jwt.sign(payload, config.get('jwtSecret'), {
+      expiresIn: 360000
+    }, (err, token) => {
+      if(err) throw err;
+      res.json({token});
+    });
 
   } catch(err) {
     console.error(err.message);
